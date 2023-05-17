0eb52406d07ecb199e433d067677918dca3faee5
beenar-0 <dncastledn@gmail.com>
2023-05-17 19:54:03 +0300

    17.05.2023

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,30 +3,40 @@ import MyInput from "./UI/MyInput/MyInput";
 import MyButton from "./UI/MyButton/MyButton";
 import MySelect from "./UI/MySelect/MySelect";
 import PostService from "../API/PostService";
-import {logDOM} from "@testing-library/react";
 
 const PostForm = ({loading, create, fetchPosts}) => {
 
     const [post, setPost] = useState({name: "", description: "", price:"", img:"", type:""})
+    // const [validationError, setValidationError] = useState([])
 
     function addNewPost(e) {
         e.preventDefault()
-        loading = true
-        const newPost = {
-            ...post,
-        }
-        PostService.addPost(newPost)
-            .then(()=>{
-                create(newPost)
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-            .finally(()=>{
-                loading = false
-                fetchPosts()
-                setPost({name: "", description: "", price:"", img:"", type:""})
-            })
+        // setValidationError([])
+        const checkName = new RegExp('^([A-Za-z])+$')
+        const checkDescription = new RegExp('^([A-Za-z\\s0-9])+$')
+        const checkPrice = new RegExp('^([0-9])+$')
+        const checkURL = new RegExp('^https?:\\/\\/.+\\.(jpg|jpeg|png|webp|avif|svg)$')
+        // if (!checkName.test(post.name)) setValidationError([...validationError, 'Incorrect name!'])
+        // if (!checkDescription.test(post.description)) setValidationError([...validationError, 'Incorrect description!'])
+        // if (!checkPrice.test(post.price)) setValidationError([...validationError, 'Incorrect price!'])
+        // if (!checkURL.test(post.img)) setValidationError([...validationError, 'Incorrect image link!'])
+        // if (post.type === '') setValidationError([...validationError, 'Chose type!'])
+            loading = true
+            const newPost = {
+                ...post,
+            }
+            PostService.addPost(newPost)
+                .then(()=>{
+                    create(newPost)
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+                .finally(()=>{
+                    loading = false
+                    fetchPosts()
+                    setPost({name: "", description: "", price:"", img:"", type:""})
+                })
     }
 
 
@@ -79,6 +89,7 @@ const PostForm = ({loading, create, fetchPosts}) => {
                     return setPost({...post, type:selectedType})
                 }}
             />
+            {/*{validationError.length > 0 && <div>{validationError.join('\n')}</div>}*/}
             <MyButton onClick={addNewPost}>Add</MyButton>
         </form>
     );
